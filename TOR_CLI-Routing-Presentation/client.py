d40ce1bf809a018f544817124911c81de7f1f71f
IndecentTulip <jowhite2727@gmail.com>
2024-11-19 13:37:21 -0800

    added Lab and Solution, updated client to show the route(need testing)

--- TOR_CLI-Routing-Presentation/client.py
+++ TOR_CLI-Routing-Presentation/client.py
@@ -2,6 +2,8 @@ import socket
 import socks  # PySocks
 from Crypto.PublicKey import RSA
 from Crypto.Cipher.PKCS1_OAEP import new
+from stem import Signal
+from stem.control import Controller
 
 # Encrypt the message with the server's public key
 def encrypt_with_public_key(message, public_key):
@@ -9,6 +11,19 @@ def encrypt_with_public_key(message, public_key):
     cipher = new(key)
     encrypted_message = cipher.encrypt(message.encode())
     return encrypted_message
+
+# Function to get the Tor circuit info
+def get_tor_circuit_info():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate()
+        
+        # Request circuit information
+        circuits = controller.get_circuits()
+        for circuit in circuits:
+            print(f"Circuit {circuit.id}:")
+            for path in circuit.path:
+                print(f"  - {path}")
+            print("------")
 
 # Client logic to continuously send messages to the server through Tor
 def client_program():
@@ -25,6 +40,9 @@ def client_program():
 
     # Receive the server's public key
     public_key = client_socket.recv(2048)
+
+    # Log current Tor circuit info
+    get_tor_circuit_info()
 
     while True: 
         # Prompt the user to enter a message
@@ -43,3 +61,4 @@ def client_program():
 
 if __name__ == "__main__":
     client_program()
+
